4898010c77e9b6e24f37b419e34b453490bc8963
Andre Garasic <no@mail.com>
2022-07-20 09:22:57 +0200

    new test script

--- bin/run-ci-ldc93s1_new.sh
+++ bin/run-ci-ldc93s1_new.sh
@@ -3,8 +3,8 @@
 set -xe
 
 
-epoch_count=$1
-audio_sample_rate=$2
+#epoch_count=$1
+#audio_sample_rate=$2
 
 
 # Force only one visible device because we have a single-sample dataset
@@ -15,7 +15,6 @@ python -u DeepSpeech.py --noshow_progressbar --noearly_stop \
   --train_files data/test1/test_excel.csv --train_batch_size 1 \
   --dev_files data/test1/test_excel.csv --dev_batch_size 1 \
   --test_files data/test1/test_excel.csv --test_batch_size 1 \
-  --n_hidden 100 --epochs $epoch_count \
+  --n_hidden 100 --epochs 50 \
   --max_to_keep 1 --checkpoint_dir 'DeepSpeech\data\test1' \
   --learning_rate 0.001 --dropout_rate 0.05  --export_dir 'DeepSpeech\data\test1' \
-  --audio_sample_rate ${audio_sample_rate}
