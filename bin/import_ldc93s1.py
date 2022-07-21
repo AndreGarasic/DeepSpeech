1e6d3a4b3df0bb88647d0928fc7f3c1d5d48e548
Andre Garasic <no@mail.com>
2022-07-21 13:14:13 +0200

    test a

--- bin/import_ldc93s1.py
+++ bin/import_ldc93s1.py
@@ -2,51 +2,51 @@
 import os
 import sys
 
-mport pandas
+import pandas
 
-from deepspeech_training.util.downloader import maybe_download
-
-
-def _download_and_preprocess_data(data_dir):
-    # Conditionally download data
-    LDC93S1_BASE = "LDC93S1"
-    LDC93S1_BASE_URL = "https://catalog.ldc.upenn.edu/desc/addenda/"
-    local_file = maybe_download(
-        LDC93S1_BASE + ".wav", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".wav"
-    )
-    trans_file = maybe_download(
-        LDC93S1_BASE + ".txt", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".txt"
-    )
-    print("podaci koji ns zanimaju")
-    print("local")
-    print(local_file)
-    print(type(local_file))
-    print("trans")
-    print(trans_file)
-    print(type(trans_file))
-
-    with open(trans_file, "r") as fin:
-        transcript = " ".join(fin.read().strip().lower().split(" ")[2:]).replace(
-            ".", ""
-        )
-
-    df = pandas.DataFrame(
-        data=[(os.path.abspath(local_file), os.path.getsize(local_file), transcript)],
-        columns=["wav_filename", "wav_filesize", "transcript"],
-    )
-    df.to_csv(os.path.join(data_dir, "ldc93s1.csv"), index=False)
-print("data")
-print(data_dir)
-print(type(data_dir))
-if __name__ == "__main__":
-    _download_and_preprocess_data(sys.argv[1])
-#./data/ldc93s1/LDC93S1.wav
-#<class 'str'>
-#trans
-#./data/ldc93s1/LDC93S1.txt
-#<class 'str'>
-#df = pandas.DataFrame(
-#    data=[(os.path.abspath('./data/test1/test_audio.wav'), os.path.getsize('./data/test1/test_audio.wav'), 'Upali ledice')],
-#    columns=["wav_filename", "wav_filesize", "transcript"],
-#)
-#df.to_csv(os.path.join(data_dir, "ldc93s1.csv"), index=False)
+#from deepspeech_training.util.downloader import maybe_download
+#
+#
+#def _download_and_preprocess_data(data_dir):
+#    # Conditionally download data
+#    LDC93S1_BASE = "LDC93S1"
+#    LDC93S1_BASE_URL = "https://catalog.ldc.upenn.edu/desc/addenda/"
+#    local_file = maybe_download(
+#        LDC93S1_BASE + ".wav", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".wav"
+#    )
+#    trans_file = maybe_download(
+#        LDC93S1_BASE + ".txt", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".txt"
+#    )
+#    print("podaci koji ns zanimaju")
+#    print("local")
+#    print(local_file)
+#    print(type(local_file))
+#    print("trans")
+#    print(trans_file)
+#    print(type(trans_file))
+#
+#    with open(trans_file, "r") as fin:
+#        transcript = " ".join(fin.read().strip().lower().split(" ")[2:]).replace(
+#            ".", ""
+#        )
+#
+#    df = pandas.DataFrame(
+#        data=[(os.path.abspath(local_file), os.path.getsize(local_file), transcript)],
+#        columns=["wav_filename", "wav_filesize", "transcript"],
+#    )
+#    df.to_csv(os.path.join(data_dir, "ldc93s1.csv"), index=False)
+#print("data")
+#print(data_dir)
+#print(type(data_dir))
+#if __name__ == "__main__":
+#    _download_and_preprocess_data(sys.argv[1])
+##./data/ldc93s1/LDC93S1.wav
+##<class 'str'>
+##trans
+##./data/ldc93s1/LDC93S1.txt
+##<class 'str'>
+df = pandas.DataFrame(
+    data=[(os.path.abspath('./data/test1/test_audio.wav'), os.path.getsize('./data/test1/test_audio.wav'), 'Upali ledice')],
+    columns=["wav_filename", "wav_filesize", "transcript"],
+)
+df.to_csv(os.path.join('./data/test1', "test.csv"), index=False)
