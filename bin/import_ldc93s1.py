1caf04667d9e467f1801aa498683341a4df80dbd
Andre Garasic <no@mail.com>
2022-07-21 11:34:40 +0200

    test custom import script

--- bin/import_ldc93s1.py
+++ bin/import_ldc93s1.py
@@ -4,29 +4,30 @@ import sys
 
 import pandas
 
-from deepspeech_training.util.downloader import maybe_download
+#from deepspeech_training.util.downloader import maybe_download
 
 
-def _download_and_preprocess_data(data_dir):
-    # Conditionally download data
-    LDC93S1_BASE = "LDC93S1"
-    LDC93S1_BASE_URL = "https://catalog.ldc.upenn.edu/desc/addenda/"
-    local_file = maybe_download(
-        LDC93S1_BASE + ".wav", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".wav"
-    )
-    trans_file = maybe_download(
-        LDC93S1_BASE + ".txt", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".txt"
-    )
-    with open(trans_file, "r") as fin:
-        transcript = " ".join(fin.read().strip().lower().split(" ")[2:]).replace(
-            ".", ""
-        )
-
+#def _download_and_preprocess_data(data_dir):
+#    # Conditionally download data
+#    LDC93S1_BASE = "LDC93S1"
+#    LDC93S1_BASE_URL = "https://catalog.ldc.upenn.edu/desc/addenda/"
+#    local_file = maybe_download(
+#        LDC93S1_BASE + ".wav", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".wav"
+#    )
+#    trans_file = maybe_download(
+#        LDC93S1_BASE + ".txt", data_dir, LDC93S1_BASE_URL + LDC93S1_BASE + ".txt"
+#    )
+#    with open(trans_file, "r") as fin:
+#        transcript = " ".join(fin.read().strip().lower().split(" ")[2:]).replace(
+#            ".", ""
+#        )
+local_file='test_audio.wav'
+data_dir='test1'
     df = pandas.DataFrame(
-        data=[(os.path.abspath(local_file), os.path.getsize(local_file), transcript)],
+        data=[(os.path.abspath(local_file), os.path.getsize(local_file), "Upali ledice")],
         columns=["wav_filename", "wav_filesize", "transcript"],
     )
-    df.to_csv(os.path.join(data_dir, "ldc93s1.csv"), index=False)
+    df.to_csv(os.path.join(data_dir, "proba_test.csv"), index=False)
 
 
 if __name__ == "__main__":
